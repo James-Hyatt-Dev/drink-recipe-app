695afe792cd7a22a662e72961d149671bb21c03b
prodigysurge <boatinglife6@yahoo.com>
2021-01-31 20:04:40 -0500

    def self.get_drink_by_name in api.rb

--- lib/api.rb
+++ lib/api.rb
@@ -1,4 +1,13 @@
 class Api 
 
+    @@apikey = "9973553"
+    
+    def self.get_drink_by_liquor
+        url = "https://www.thecocktaildb.com/api/json/v2/#{@@api_key}/filter.php?i=#{liquor}"
+        response = HTTParty.get(url)
+        liquor_name_hash = 
+        Drinks.new(liquor_name_hash)
+    end
+    
 
 end
--- lib/drinks.rb
+++ lib/drinks.rb
@@ -1,4 +1,8 @@
 class Drinks 
+    attr_accessor :names, :ids
 
+    @@all = []
+
+    def initialize(hash)
 
 end 
